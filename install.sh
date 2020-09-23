f6862100b2c000b26403189e72feb3da3dce63b5
Paul Adams <paul@thoughtcriminal.co.uk>
2020-09-23 17:32:36 +0300

    correct service name in instructions, fixes #4

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp dynamic-cloud-dns-client@.service /etc/systemd/system
 
 if [ -n "$1" ]; then
   cp dynamic_cloud_dns_client_your.host "/etc/dynamic_cloud_dns_client_$1"
-  echo "Edit /etc/dynamic_cloud_dns_client_$1 and then run systemctl enable dynamic_cloud_dns_client@$1.service"
+  echo "Edit /etc/dynamic_cloud_dns_client_$1 and then run systemctl enable dynamic-cloud-dns-client@$1.service"
 fi
 
 systemctl daemon-reload
